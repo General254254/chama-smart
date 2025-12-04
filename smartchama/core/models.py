b429302efe0e10b5eccaee053ee99424322499a9
General254254 <markgimajeti@gmail.com>
2025-12-04 23:22:04 +0300

    add templates

--- smartchama/core/models.py
+++ smartchama/core/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+# Chama Group
+class ChamaGroup(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.name
+
+
+class Member(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)  # links to Django user
+    group = models.ForeignKey(ChamaGroup, on_delete=models.CASCADE)
+    phone_number = models.CharField(max_length=20, blank=True)
+    
+    def __str__(self):
+        return self.user.username
+
+
+class Contribution(models.Model):
+    member = models.ForeignKey(Member, on_delete=models.CASCADE)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    date = models.DateField(auto_now_add=True)
+    
+    def __str__(self):
+        return f"{self.member.user.username} - {self.amount}"
--- smartchama/core/views.py
+++ smartchama/core/views.py
@@ -1,3 +1,38 @@
 from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate, logout
+from .forms import MemberRegistrationForm
+
 
 # Create your views here.
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("<h1>Welcome to Smart Chama App</h1>")
+
+def register(request):
+    if request.method == 'POST':
+        form = MemberRegistrationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)  # automatically log in after registration
+            return redirect('home')
+    else:
+        form = MemberRegistrationForm()
+    return render(request, 'register.html', {'form': form})
+
+def user_login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            return render(request, 'login.html', {'error': 'Invalid credentials'})
+    return render(request, 'login.html')
+
+def user_logout(request):
+    logout(request)
+    return redirect('login')
